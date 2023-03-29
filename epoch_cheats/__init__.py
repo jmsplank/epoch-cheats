a8c8d5fc079edfcaa0cae5e02ad9885a3d34ecdd
James Plank <plankjames@gmail.com>
2023-03-29 20:35:57 +0100

    fix: better docstring and explicit imports
    imports to make py.typed happy

--- epoch_cheats/__init__.py
+++ epoch_cheats/__init__.py
@@ -1,3 +1,4 @@
-"""Nothing here."""
-from .cli import epoch_cheats
-from .deck_parse import get_deck_constants, get_deck_constants_sym
+"""Initialise epoch_cheats and import the main functions."""
+from .cli import epoch_cheats as epoch_cheats
+from .deck_parse import evaluate_deck as evaluate_deck
+from .deck_parse import get_deck_constants as get_deck_constants
