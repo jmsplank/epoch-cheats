e6b39f1411d83b2fce39f4bfe2ecda1c7928f9ae
James Plank <plankjames@gmail.com>
2023-08-10 14:57:09 +0100

    docs: more info

--- epoch_cheats/__init__.py
+++ epoch_cheats/__init__.py
@@ -1,6 +1,6 @@
 """Initialise epoch_cheats and import the main functions."""
 
 from .cli import epoch_cheats as epoch_cheats
-from .deck_parse import evaluate_deck as evaluate_deck
+from .deck_parse import evaluate_deck as evaluate_deck  # <- This is the main function
 from .deck_parse import get_deck_constants as get_deck_constants
 from .deck_parse import validate_deck as validate_deck
